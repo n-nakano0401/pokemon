47bc0c8311c830a4142b89d06a5f305216a91d60
n-nakano <n-nakano@matsusaka.co.jp>
2022-05-13 13:34:27 +0900

    add speed

--- MainTask/Battle.java
+++ MainTask/Battle.java
@@ -3,25 +3,32 @@ import java.util.*;
 import monster.Monster;
 
 public class Battle {
+	private Random rand = new Random();
+	
 	private String pl_name = "まえだ";
 	private String en_name = "いまにし";
 	private UImanager ui;
 	
 	private int pl_mon_HP = 0;
 	private int en_mon_HP = 0;
+	private int first = 0;//先手をどちらがとるか判定
 	
 	//戦闘時のフローについては全部ここに書く
 	public static void main(String[] args){
+		//-----------------変数の設定-----------------
+		//仕様上の処理
 		Battle battle = new Battle();
 		Scanner sc = new Scanner(System.in);
 
 		//プレイヤーのポケモンを参照(敵も味方も)
-		Monster pl_mon = new Monster("ピカチュウ",60,15);
-		Monster en_mon = new Monster("フシギダネ",50,10);
+		Monster pl_mon = new Monster("ピカチュウ",60,15,30);
+		Monster en_mon = new Monster("フシギダネ",50,10,20);
 		
 		//コマンドライン上の演出はuiで"すべて"管理する
 		battle.ui = new UImanager(battle.pl_name, battle.en_name, pl_mon.getName(),en_mon.getName());
 		
+		
+		//------------------ここから処理-------------------
 		//一番先頭にいるポケモンを出す(敵も味方も)
 		battle.setBattleHP(pl_mon.getHp(),en_mon.getHp());
 		battle.ui.battleEntry();
@@ -30,17 +37,28 @@ public class Battle {
 		while(battle.pl_mon_HP>0){
 			battle.ui.battleVisual(battle.pl_mon_HP, pl_mon.getHp(), battle.en_mon_HP, en_mon.getHp());
 			String menuchoice = sc.next();
+
 			
 			//選択
 			if(menuchoice.matches("[+-]?\\d*(\\.\\d+)?")){
+				//どちらが先手をとれるか判定する
+				battle.first = battle.firstMoveSetter(pl_mon.getSpeed(),en_mon.getSpeed());
+
 				if(menuchoice.equals("1")){
-					battle.ui.attack(0,pl_mon.getPower());
-					//技を選択し、ダメージの処理をする
-					battle.en_mon_HP = battle.en_mon_HP - pl_mon.getPower();
+					if(battle.first == 0){
+						battle.ui.attack(0,pl_mon.getPower());
+						//技を選択し、ダメージの処理をする
+						battle.en_mon_HP = battle.en_mon_HP - pl_mon.getPower();
+					}else if(battle.first == 1){
+						battle.enemyTactics(pl_mon.getName(),en_mon.getName(),en_mon.getPower());
+					}
+					
 				}else if(menuchoice.equals("2")){
-					battle.ui.useItem();					
+					battle.ui.useItem();
+					battle.first = 0;
 				}else{
 					battle.ui.tempMessage();
+					battle.first = 0;
 				}
 			}else{battle.ui.inputException();}
 			
@@ -48,7 +66,13 @@ public class Battle {
 			//このとき敵HPが0になったら戦闘終了である
 			battle.winCheck(pl_mon.getName(),en_mon.getName());
 			//敵のターン
-			battle.enemyTactics(pl_mon.getName(),en_mon.getName(),en_mon.getPower());
+			if(battle.first == 1){
+				battle.ui.attack(0,pl_mon.getPower());
+				//技を選択し、ダメージの処理をする
+				battle.en_mon_HP = battle.en_mon_HP - pl_mon.getPower();
+			}else if(battle.first == 0){
+				battle.enemyTactics(pl_mon.getName(),en_mon.getName(),en_mon.getPower());
+			}
 			//このとき味方HPが0になったら戦闘終了である
 			battle.winCheck(pl_mon.getName(),en_mon.getName());
 			System.out.println("");
@@ -63,7 +87,6 @@ public class Battle {
 	
 	//終了処理
 	public void winCheck(String pl_mon, String en_mon){
-		Scanner sc = new Scanner(System.in);
 		if(pl_mon_HP <= 0){
 			ui.loseMessage();
 			System.exit(0);
@@ -78,5 +101,17 @@ public class Battle {
 	public void enemyTactics(String pl_mon, String en_mon, int enPower){
 		ui.attack(1,enPower);
 		pl_mon_HP = pl_mon_HP - enPower;
+	}
+	
+	//先手をとるほうを決定する処理
+	public int firstMoveSetter(int pl_monS, int en_monS){
+		if(pl_monS > en_monS){
+			return 0;
+		}else if(pl_monS < en_monS){
+			return 1;
+		}else{
+			return rand.nextInt(2);
+		}
+		
 	}
 }
